91990f9d07749c2a3ecd357590cd6e60ec06013a
RV <rock1990vishal@gmail.com>
2025-08-08 20:25:11 +0530

    adding credentials file and aws v3

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,3 +1,16 @@
-# AWS SDK v3 configuration
+# AWS SDK v3 configuration for Paperclip compatibility
 # The new AWS SDK v3 is configured through environment variables or credentials file
-# No explicit configuration needed here as it's handled by the aws-sdk-s3 gem
+
+# Required for Paperclip compatibility with aws-sdk-s3
+if Rails.application.credentials.dig(:aws, :access_key_id).present?
+  Aws.config.update({
+    region: Rails.application.credentials.dig(:aws, :region) || 'us-east-1',
+    credentials: Aws::Credentials.new(
+      Rails.application.credentials.dig(:aws, :access_key_id),
+      Rails.application.credentials.dig(:aws, :secret_access_key)
+    )
+  })
+end
+
+# Paperclip S3 configuration for aws-sdk-s3 compatibility
+Paperclip::Attachment.default_options[:s3_region] = Rails.application.credentials.dig(:aws, :region) || 'us-east-1'
--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,14 +1,18 @@
-require 'raven'
-require 'resque/failure/multiple'
-require 'resque/failure/redis'
-require 'resque-sentry'
+# Sentry configuration for error tracking
+# Updated to use sentry-ruby instead of deprecated sentry-raven
 
 if Rails.env.production?
-  Resque::Failure::Sentry.logger = "resque"
+  require 'resque/failure/multiple'
+  require 'resque/failure/redis'
+  require 'resque-sentry'
 
+  Resque::Failure::Sentry.logger = "resque"
   Resque::Failure::Multiple.classes = [Resque::Failure::Redis, Resque::Failure::Sentry]
   Resque::Failure.backend = Resque::Failure::Multiple
-  Raven.configure do |config|
+
+  Sentry.init do |config|
     config.dsn = CRMConfig.SENTRY_DSN
+    config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+    config.traces_sample_rate = 0.5
   end
 end
