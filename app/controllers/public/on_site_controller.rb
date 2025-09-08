fde43a7e24ab81d13c0dc001f93437b96cb2cdb9
RV <rock1990vishal@gmail.com>
2025-09-08 19:59:16 +0530

    fixing

--- app/controllers/public/on_site_controller.rb
+++ app/controllers/public/on_site_controller.rb
@@ -39,8 +39,6 @@ module Public
         @lead.email = origin_lead.email if @lead.email.blank?
         @lead.mobile = origin_lead.mobile if @lead.mobile.blank?
       end
-      @lead.status_id = @company.expected_site_visit_id if @lead.tentative_visit_planned.present?
-      @lead.source_id = ::Source.cp_sources.first.id if @lead.source_id.blank?
       if @lead.save
         render json: {message: "Visit Scheduled", data: {lead_no: @lead.reload.lead_no}}, status: 200 and return
       else
@@ -73,6 +71,8 @@ module Public
     def create_new_lead input_params
       # Create new lead with magic fields using helper method
       lead = Lead.build_with_magic_fields(@company, input_params)
+      lead.status_id = @company.expected_site_visit_id if lead.tentative_visit_planned.present?
+      lead.source_id = ::Source.cp_sources.first.id if lead.source_id.blank?
       lead
     end
   end
