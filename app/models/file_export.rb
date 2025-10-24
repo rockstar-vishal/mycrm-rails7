5edf7f3e69369fba9d76f96ce039c04b182a1e03
RV <rock1990vishal@gmail.com>
2025-10-24 14:48:01 +0530

    fix

--- app/models/file_export.rb
+++ app/models/file_export.rb
@@ -3,7 +3,7 @@ class FileExport < ActiveRecord::Base
   belongs_to :company
 
   scope :todays, -> {where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)}
-  scope :week_file, -> {where(created_at: Time.zone.now-1.week..Time.zone.now.end_of_day)}
+  scope :week_file, -> {where(created_at: Time.zone.now-1.month..Time.zone.now.end_of_day)}
 
 
   def csv_data
